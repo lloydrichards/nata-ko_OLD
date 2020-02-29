87ca10d406b13da070c10cf0712b2f571cb1a110
LloydRichardsIntern <59728961+LloydRichardsIntern@users.noreply.github.com>
2020-02-29 13:26:56 +0100

    content: Added Gallery
    Added gallery images with some cross fading.  only 4 images so far, need to rework the css to add in the 2 last images.

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Image1 from '../images/nata-ko1.jpg';
+import Image2 from '../images/nata-ko2.jpg';
+import Image3 from '../images/nata-ko3.jpg';
+import Image4 from '../images/nata-ko4.jpg';
+import Image5 from '../images/nata-ko5.jpg';
+import Image6 from '../images/nata-ko6.jpg';
 
 function Gallery() {
   return (
-    <div>
-      <p>Gallery</p>
+    <div id='cf'>
+      <img className='bottom' src={Image1} />
+      <img className='bottom' src={Image2} />
+      <img className='bottom' src={Image3} />
+      <img className='bottom' src={Image4} />
     </div>
   );
 }
