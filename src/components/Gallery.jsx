83dbce44ead5047a37866423b2a783d8c135dacb
LloydRichardsIntern <59728961+LloydRichardsIntern@users.noreply.github.com>
2020-04-26 16:51:56 +0200

    content: Updated Images

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,19 +1,17 @@
-import React from 'react';
-import { css } from 'emotion';
-import Image1 from '../images/nata-ko1.jpg';
-import Image2 from '../images/nata-ko2.jpg';
-import Image3 from '../images/nata-ko3.jpg';
-import Image4 from '../images/nata-ko4.jpg';
-import Image5 from '../images/nata-ko5.jpg';
-import Image6 from '../images/nata-ko6.jpg';
+import React from "react";
+import { css } from "emotion";
+import Desktop1 from "../images/nata-ko1.jpg";
+import Desktop2 from "../images/nata-ko2.jpg";
+import Desktop3 from "../images/nata-ko3.jpg";
+import Desktop4 from "../images/nata-ko4.jpg";
 
 function Gallery() {
   return (
-    <div style={{ height: '90vh', overflow: 'hidden' }} id='cf'>
-      <img className='bottom' alt='Front Page Image 1' src={Image1} />
-      <img className='bottom' alt='Front Page Image 2' src={Image2} />
-      <img className='bottom' alt='Front Page Image 3' src={Image3} />
-      <img className='bottom' alt='Front Page Image 4' src={Image4} />
+    <div style={{ height: "90vh", overflow: "hidden" }} id="cf">
+      <img className="bottom" alt="Front Page Image 1" src={Desktop1} />
+      <img className="bottom" alt="Front Page Image 2" src={Desktop2} />
+      <img className="bottom" alt="Front Page Image 3" src={Desktop3} />
+      <img className="bottom" alt="Front Page Image 4" src={Desktop4} />
     </div>
   );
 }
